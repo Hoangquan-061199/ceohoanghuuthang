2e1b0a31ac0da7b39a005ce259e9e4cceb50a096
hoangquan-061199 <hoangquan061199@gmail.com>
2022-10-05 17:38:21 +0700

    slider slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,54 @@ $('.menu-mobile .icon-menu').click(() => {
 });
 
 $(window).scroll(() => {
-  if ($(window).width() > 992) {
+  if ($(window).width() >= 992) {
     let heightHeader = header.height();
     if ($(this).scrollTop() > heightHeader) {
       header.addClass('active');
     } else {
       header.removeClass('active');
     }
+  } else {
+    header.removeClass('active');
   }
+});
+
+$('.blog .slider').owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 34,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    900: {
+      items: 2.5,
+    },
+    1100: {
+      items: 3,
+    },
+  },
+  dots: true,
+});
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  variableWidth: true,
+  asNavFor: '.slider-nav',
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
 });
